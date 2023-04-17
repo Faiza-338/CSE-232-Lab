fc8db90ef693df796f6c47cb998b4e1817918098
Faiza Khandoker <109524293+Faiza-338@users.noreply.github.com>
2023-04-17 09:24:52 +0600

    Update linked list travarsel.c

--- Linked list/linked list travarsel.c
+++ Linked list/linked list travarsel.c
@@ -1,34 +1,37 @@
 #include <stdio.h>
-struct node{
-  int data;
-  struct node *next;
+#include <stdlib.h>
 
+struct node {
+    int data;
+    struct node *next;
 };
-struct node *head,*newnode,*temp;
-//ptr=malloc(cast_type*sizeof(data type))
-//malloc-allocates memory dynamycally  and creates a new node
-newnode=malloc(sruct node*sizeof(struct node));
-head=0;int choice;
-printf("Enter data:/n");
-scanf("%d",&newnode->data)
-newnode->next=0;
- while(choice){
- if(head==0){
-     head=temp=newnode;
- }
- else{
-     temp->next=newnode;
-     temp=newnode;
 
- }
- printf("Do you want to continue?(0/1)");
- scanf("%d",&choice);
-}
-temp=head;
-while(temp!=0){
-    printf("%d",temp->data);
-    temp=temp->next
-}
+int main() {
+    struct node *head = NULL, *newnode, *temp;
+    int choice = 1;
 
+    while (choice) {
+        newnode = (struct node*)malloc(sizeof(struct node));
+        printf("Enter data:\n");
+        scanf("%d", &newnode->data);
+        newnode->next = 0;
 
+        if (head == 0) {
+            head = temp = newnode;
+        } else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+
+        printf("Do you want to continue? (0/1)\n");
+        scanf("%d", &choice);
+    }
+
+    temp = head;
+    while (temp != 0) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
 }
